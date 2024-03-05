040a9f5f4ff5f7f0acae8489e6b7f42ef39f8e4f
문준석 <junsuk817@naver.com>
2024-03-05 17:16:55 +0900

    첫커커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+
+import esriConfig from "@arcgis/core/config.js";
+import Map from "@arcgis/core/Map.js";
+import MapView from "@arcgis/core/views/MapView.js";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <strong>GIS Map Test</strong>
+            </li>
+            <li>main</li>
+            <li>awt</li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        <section>
+          <MappingApp></MappingApp> 
+        </section>
+      </main>
+      <footer>
+        <section className='footerSection'>
+          <p>이메일 : junsuk817@naver.com</p>
+          <p>깃허브 : https://github.com/mooon610</p>
+          <p>전화번호 : 010-6514-6445</p>
+        </section>
+      </footer>
+    </>
   );
+}
+
+function MappingApp() {
+  useEffect(()=>{
+
+  },[])
+  return <div id="viewDiv"></div>
 }
 
 export default App;
